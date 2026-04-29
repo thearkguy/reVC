344cfde31b0554871f1f349318d4fd7c05820f37
ZeptoBST <57198468+ZeptoBST@users.noreply.github.com>
2026-04-29 16:54:44 -0400

    Update WeaponInfo.cpp
    Added landmark.

--- src/weapons/WeaponInfo.cpp
+++ src/weapons/WeaponInfo.cpp
@@ -30,7 +30,7 @@ uint16 CWeaponInfo::ms_aReloadSampleTime[WEAPONTYPE_TOTALWEAPONS] =
 	0,			// ROCKET
 	250,		// COLT45
 	250,		// PYTHON
-	150,		// SHOTGUN		// ZAdd: Was "650". Fixes reloading sound not playing.
+	150,		// SHOTGUN		// ZAdd: Was "650". Fixes reloading sound not playing. #AudioFix
 	650,		// SPAS12 SHOTGUN
 	650,		// STUBBY SHOTGUN
 	400,		// TEC9
